8bf77148696cd141e86a449b91929b73aca7955d
Matthew Mercuri <mercurimatthew@gmail.com>
2022-12-13 19:06:06 -0500

    cleaned up eslint errors

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,13 @@
+/* eslint-disable no-console */
 import express, { Express } from "express";
 
-const app: Express = express()
-const port: number = 3000
+const app: Express = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`)
-})
+  console.log(`Server is listening on port ${port}`);
+});
