f3799a92e44d22b04c6dfd331f9395f94dfa4892
Matthew Mercuri <mercurimatthew@gmail.com>
2022-12-14 00:10:52 -0500

    typing historical data query options

--- src/services/web/web.ts
+++ src/services/web/web.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
+import type { ValidTickers } from "./web.types";
 
 axios.defaults.baseURL = "http://localhost:3000";
 
-export function getHistoricalData(ticker: string) {
+export function getHistoricalData(ticker: ValidTickers) {
   const reqUrl = "/historical-data";
   return axios.get(reqUrl, { params: { ticker } });
 }
---
+++ src/services/web/web.types.ts
@@ -0,0 +1,3 @@
+// INFO: we can actually use generics to make a case
+// insensitive type
+export type ValidTickers = "QQQ" | "VTI" | "SPY" | "ABALX";
