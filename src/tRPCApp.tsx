99b317c5b7c4baaebb9b3e77ea115f7682c152f4
Matthew Mercuri <mercurimatthew@gmail.com>
2022-12-14 11:33:26 -0500

    cleanup before using tRPC

--- src/tRPCApp.tsx
+++ src/tRPCApp.tsx
@@ -1,36 +1,20 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import { useState } from "react";
-import Select from "react-select";
 import { Chart } from "./Chart";
-import { tickerSelections } from "./services/tickers";
 import { trpc } from "./services/trpc";
-import type { OptionType } from "./types/select.types";
 import type { HistoricalTickerData } from "./types/tickers.types";
 
 const Content: React.FC = () => {
-  const [selectedTicker, setSelectedTicker] = useState<OptionType>();
-  const [historicalData, setHistoricalData] = useState<
-    HistoricalTickerData | undefined
-  >();
-
+  const historicalData: HistoricalTickerData = [];
   // EXAMPLE QUERY:
   // const { data } = trpc.hello.useQuery({ text: "Stephen" });
   // console.log(data?.greeting);
 
   return (
     <div className="app">
-      <Select
-        className="app__dropdown"
-        defaultValue={selectedTicker}
-        // onChange={fetchHistoricalData}
-        options={tickerSelections}
-      />
       {historicalData && (
-        <Chart
-          historicalData={historicalData}
-          title={selectedTicker?.value || "Price"}
-        />
+        <Chart historicalData={historicalData} title="Price" />
       )}
     </div>
   );
