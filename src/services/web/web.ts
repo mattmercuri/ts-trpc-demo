594010523f6b24b3b0ce2d8410adc632f4e1454c
Matthew Mercuri <mercurimatthew@gmail.com>
2022-12-14 00:03:52 -0500

    added axios call; added web types folder

--- src/services/web/web.ts
+++ src/services/web/web.ts
@@ -1,1 +1,8 @@
 import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:3000";
+
+export function getHistoricalData(ticker: string) {
+  const reqUrl = "/historical-data";
+  return axios.get(reqUrl, { params: { ticker } });
+}
