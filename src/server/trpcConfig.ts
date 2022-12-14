f517f653e177c8e0673a9c6c09e241386406e6ed
Matthew Mercuri <mercurimatthew@gmail.com>
2022-12-14 11:04:41 -0500

    configured test trpc endpoint

--- src/server/trpcConfig.ts
+++ src/server/trpcConfig.ts
@@ -3,7 +3,23 @@ import { z } from "zod";
 
 export const t = initTRPC.create();
 
-export const appRouter = t.router({});
+const publicProcedure = t.procedure;
+
+export const appRouter = t.router({
+  hello: publicProcedure
+    .input(
+      z
+        .object({
+          text: z.string(),
+        })
+        .optional()
+    )
+    .query(({ input }) => {
+      return {
+        greeting: `hello ${input?.text ?? "world"}`,
+      };
+    }),
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
