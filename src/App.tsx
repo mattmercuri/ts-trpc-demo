ce33a06091c878bf6d7c87939e054d48a48855b9
Matthew Mercuri <mercurimatthew@gmail.com>
2022-12-14 08:52:10 -0500

    configured dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
+import Select from "react-select";
+import { tickerSelections } from "./services/tickers";
 import { getHistoricalData } from "./services/web/web";
+import { ValidTickers } from "./types/tickers.types";
 
 function App() {
+  const [selectedTicker, setSelectedTicker] = useState<ValidTickers | null>(
+    tickerSelections[0].value
+  );
+
   const fetchHistoricalData = () => {
     getHistoricalData("QQQ").then((data) => {
       console.log(data);
@@ -9,6 +17,11 @@ function App() {
 
   return (
     <div>
+      <Select
+        defaultValue={selectedTicker}
+        onChange={setSelectedTicker}
+        options={tickerSelections}
+      />
       <button onClick={fetchHistoricalData}>Get Data</button>
     </div>
   );
---
+++ src/services/tickers.ts
@@ -0,0 +1,24 @@
+import type { TickerObject } from "../types/tickers.types";
+
+export const TickerInfoObject: TickerObject = {
+  ABALX: {
+    name: "American Balanced Fund",
+    ticker: "ABALX",
+  },
+  QQQ: {
+    name: "NASDAQ 100",
+    ticker: "QQQ",
+  },
+  SPY: {
+    name: "S&P 500",
+    ticker: "SPY",
+  },
+  VTI: {
+    name: "Total US Market",
+    ticker: "VTI",
+  },
+};
+
+export const tickerSelections = Object.values(TickerInfoObject).map(
+  ({ ticker, name }) => ({ value: ticker, label: name, options: [] })
+);
