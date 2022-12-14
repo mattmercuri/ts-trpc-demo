727a90cf3f22f0cd059ea292fa77d8827ea87301
Matthew Mercuri <mercurimatthew@gmail.com>
2022-12-14 10:07:16 -0500

    storing historical data in state

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,24 @@ import { Chart } from "./Chart";
 import { tickerSelections } from "./services/tickers";
 import { getHistoricalData } from "./services/web/web";
 import type { OptionType } from "./types/select.types";
-import type { ValidTicker } from "./types/tickers.types";
+import { HistoricalTickerData, ValidTicker } from "./types/tickers.types";
 
 function App() {
   const [selectedTicker, setSelectedTicker] = useState<OptionType | null>(
     tickerSelections[0]
   );
+  const [historicalData, setHistoricalData] = useState<
+    HistoricalTickerData | undefined
+  >();
 
   const fetchHistoricalData = (selection: OptionType | null) => {
     setSelectedTicker(selection);
 
-    if (!selection?.value) {
-      return;
+    if (selection?.value) {
+      getHistoricalData(selection.value as ValidTicker).then(({ data }) => {
+        setHistoricalData(data);
+      });
     }
-
-    getHistoricalData(selection.value as ValidTicker).then(({ data }) => {
-      console.log(data);
-    });
   };
 
   return (
