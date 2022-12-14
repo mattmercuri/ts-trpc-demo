f035b567878200b7494e81124a1fa50698889201
Matthew Mercuri <mercurimatthew@gmail.com>
2022-12-14 08:32:10 -0500

    added react-select; added types for ticker object

--- src/types/tickers.types.ts
+++ src/types/tickers.types.ts
@@ -1,3 +1,10 @@
 // INFO: we can actually use generics to make a case
 // insensitive type
 export type ValidTickers = "QQQ" | "VTI" | "SPY" | "ABALX";
+
+export type TickerInfo = {
+  ticker: ValidTickers;
+  name: string;
+};
+
+export type TickerObject = { [key in ValidTickers]: TickerInfo };
