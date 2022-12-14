a11410fa048bcc54150845a4eff875eb96b26438
Matthew Mercuri <mercurimatthew@gmail.com>
2022-12-14 11:20:23 -0500

    new content component

--- src/tRPCApp.tsx
+++ src/tRPCApp.tsx
@@ -1,13 +1,38 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import { useState } from "react";
+import Select from "react-select";
+import { Chart } from "./Chart";
+import { tickerSelections } from "./services/tickers";
 import { trpc } from "./services/trpc";
+import type { OptionType } from "./types/select.types";
+import type { HistoricalTickerData } from "./types/tickers.types";
 
-type AppType = {
-  children: React.ReactNode;
+const Content: React.FC = () => {
+  const [selectedTicker, setSelectedTicker] = useState<OptionType>();
+  const [historicalData, setHistoricalData] = useState<
+    HistoricalTickerData | undefined
+  >();
+
+  return (
+    <div className="app">
+      <Select
+        className="app__dropdown"
+        defaultValue={selectedTicker}
+        // onChange={fetchHistoricalData}
+        options={tickerSelections}
+      />
+      {historicalData && (
+        <Chart
+          historicalData={historicalData}
+          title={selectedTicker?.value || "Price"}
+        />
+      )}
+    </div>
+  );
 };
 
-const App: React.FC<AppType> = ({ children }) => {
+const App: React.FC = () => {
   const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
@@ -20,7 +45,9 @@ const App: React.FC<AppType> = ({ children }) => {
   );
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <Content />
+      </QueryClientProvider>
     </trpc.Provider>
   );
 };
